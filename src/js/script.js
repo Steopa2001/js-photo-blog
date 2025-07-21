571c99b779468fc42bc218d0cc36b9b5267bb391
Steopa2001 <moraru495@gmail.com>
2025-07-21 18:49:47 +0200

    feat: make AJAX call with Axios

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,15 @@
 // Recupero il contenitore delle card
-const fotoList = document.getElementById('foto-list');
+const fotoList = document.getElementById("foto-list");
 
 // Funzione per generare le card dinamicamente
-function generaFoto () {
+function generaFoto() {
 
-};
+  // Svuoto la lista
+  fotoList.innerHTML = "";
+
+    // Chiamata AJAX con Axios
+    axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp) => {
+
+    });
+
+}
