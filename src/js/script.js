7c50dddc835d97f24069ee2323df557b6c9218a0
Steopa2001 <moraru495@gmail.com>
2025-07-21 19:20:37 +0200

    fix: replace the forEach loop with a classic for loop

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,26 @@
-// Recupero il contenitore delle card
+// Recupero il contenitore delle card dal DOM
 const fotoList = document.getElementById("foto-list");
 
 // Funzione per generare le card dinamicamente
 function generaFoto() {
-  // Svuoto la lista
+  // Pulisco il contenuto precedente dentro fotoList, così evito di duplicare le card
   fotoList.innerHTML = "";
 
-  // Chiamata AJAX con Axios
+  // Faccio una chiamata HTTP GET all'API usando Axios
+  // L'API restituisce un array di oggetti con info sulle foto
   axios.get("https://lanciweb.github.io/demo/api/pictures/").then((resp) => {
+    // Salvo in fotoArray i dati ricevuti dalla risposta
+    // resp.data contiene l'array di foto
     const fotoArray = resp.data;
 
-    //Ciclo sull'array e creo una card per ogni oggetto
-    fotoArray.forEach((foto) => {
+    // Per debugging: stampo l'array di foto in console così vedo cosa arriva
+    console.log(fotoArray);
+
+    // Ciclo classico for per scorrere l'array delle foto
+    for (let i = 0; i < fotoArray.length; i++) {
+      const foto = fotoArray[i]; // prendo la singola foto
+
+      // Aggiungo al contenitore fotoList una struttura HTML per ogni foto:
       fotoList.innerHTML += `
           <div class="col-10 col-md-5 col-lg-4">
             <div class="card position-relative h-100" style="border-radius: 0">
@@ -24,9 +33,9 @@ function generaFoto() {
             </div>
           </div>
         `;
-    });
+    }
   });
-};
+}
 
 // Eseguo la funzione al caricamento della pagina
 generaFoto();
